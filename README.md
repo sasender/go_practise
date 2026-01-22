dbe191773ba0a7eec4e9507609cc79b0733ffea9
sasender <sasender123@gmail>
2026-01-22 12:59:46 +0530

    Adding-switchin-examples

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Basic_Understanding of Go language 
 https://medium.com/@jamal.kaksouri/a-comprehensive-guide-to-pointers-in-go-4acc58eb1f4d
 
+https://www.freecodecamp.org/news/key-golang-concepts-for-beginner-go-devs/
---
+++ switch_examples/01-switch-examples.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	day := "Tuesday"
+	switch day {
+	case "Monday":
+		fmt.Println("It's Monday!")
+	case "Tuesday":
+		fmt.Println("It's Tuesday!")
+	default:
+		fmt.Println("It's another day.")
+	}
+}
+
+/*package main
+
+import "fmt"
+*/
+
+// func main() {
+// 	day := "Sunday"
+// 	switch day {
+// 	case "Monday":
+// 		fmt.Println("It's Monday!")
+// 	case "Friday":
+// 		fmt.Println("It's Friday!")
+// 	default:
+// 		fmt.Println("It's another day.")
+// 	}
+
+// }
