07f48f12066e3feea3165f82347d6308669e2de1
sasender <sasender123@gmail>
2026-01-23 13:23:21 +0530

    adding-readme

--- Assignments/01-hellow-example.go
+++ Assignments/01-hellow-example.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	var name string
 	fmt.Print("Enter your Name:")
+	//after we given the input its automatically stored in name variable by helping of scan
 	fmt.Scan(&name)
 
 	date := time.Now().Format("02-01-2006")
