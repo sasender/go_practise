4ea5ba739796b57eb21a46776eff7d353f1f7094
sasender <sasender123@gmail>
2026-03-26 12:47:45 +0530

    added-slice-examples

---
+++ learning/slices-examples/01-slices-example.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{10, 20, 30}
+
+	s[1] = 25
+	fmt.Println(s)
+}
+
+// In this code, we have declared an array arr with 5 string elements. We then use a for loop to iterate over the array and print each element. The output will show each string in the array on a new line.
