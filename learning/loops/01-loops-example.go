dfab8f88437278bff691beb29329ad0e42419f17
sasender <sasender123@gmail>
2026-04-23 12:03:46 +0530

    adding-loops-functions

---
+++ learning/loops/01-loops-example.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Print("print numbers:")
+	for i := 1; i <= 10; i++ {
+		fmt.Printf(" %d", i)
+	}
+	fmt.Println()
+}
---
+++ learning/loops/02-loops-example.go
@@ -0,0 +1,26 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	total := 55
+// 	for i := 1; i <= 10; i++ {
+// 		//total += i
+// 	}
+
+// 	fmt.Println(total)
+// }
+
+//https://stackoverflow.com/questions/30955792/difference-between-for-loop-construct-vs-range-keyword-in-go
+
+package main
+
+import "fmt"
+
+func main() {
+	total := 86
+	for i := 1; i <= 10; i++ {
+
+	}
+	fmt.Println("total", total)
+}
